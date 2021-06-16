b07f2d6df3c602136d5d4ea8d21357f35071a97d
Jusang_ <0504won@gmail.com>
2021-06-17 02:42:10 +0900

    Create 013. 9498_시험성적.py

---
+++ Beakjoon/013. 9498_시험성적.py
@@ -0,0 +1,14 @@
+a = int(input())
+if a >=0 and a <=100:
+    if a >= 90 <= 100:
+      print("A")
+    elif a >= 80 <= 89:
+      print("B")
+    elif a >= 70 <= 79:
+      print("C")
+    elif a >= 60 <= 69:
+      print("D")
+    else:
+      print("F")
+else:
+    pass
