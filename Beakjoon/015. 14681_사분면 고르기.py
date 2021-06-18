c3eaf1f138d48cbba49e8bc71855b7ae71f887f0
Jusang_ <0504won@gmail.com>
2021-06-18 22:52:08 +0900

    Create 015. 14681_사분면 고르기.py

---
+++ Beakjoon/015. 14681_사분면 고르기.py
@@ -0,0 +1,13 @@
+x = int(input())
+if x > 0:
+  y = int(input())
+  if y > 0:
+    print("1")
+  elif y < 0:
+    print("4")
+elif x < 0:
+  y = int(input())
+  if y > 0:
+    print("2")
+  elif y < 0:
+    print("3")
